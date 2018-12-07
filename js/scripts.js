1a89064455e46f1fca5a62e4ea356166c4175250
Khizir Putcygov <k.putcygov@gmail.com>
2018-12-07 13:31:47 +0100

    Dealt with sprecial cases
    Added 9* and 4*

--- js/scripts.js
+++ js/scripts.js
@@ -1,7 +1,7 @@
 const toRoman = (num) => {  
   let stringsRes = '';
-  let decimals = [1000, 500, 100, 50, 10, 5, 1];
-  var romans = ['M', D, C, L, X, V, I];
+  let decimals = [1000, 900, 500, 400, 100, 90, 50, 40, 10, 9, 5, 4, 1];
+  let romans = ['M', 'CM', 'D', 'CD', 'C', 'XC', 'L', 'XL', 'X', 'IX', 'V', 'IV', 'I'];
   for (let i = 0; i <= decimals.length; i++) { 
     while (num % decimals[i] < num) {   
       stringsRes += romans[i]; 
@@ -17,3 +17,4 @@ document.write(`${toRoman(12)} (expected: XII)<br>`);
 document.write(`${toRoman(114)} (expected: CXIV)<br>`);  
 document.write(`${toRoman(2000)} (expected: MM)<br>`);  
 document.write(`${toRoman(149)} (expected: CXLIX)<br>`); 
+document.write(`${toRoman(1494)} (expected: MCDXCIV)<br>`); 
